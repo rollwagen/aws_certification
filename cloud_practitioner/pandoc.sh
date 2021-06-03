cf3fb99bcf8b6b732c0b4d7ea20f0b56cb3301c4
rollwagen <rollwagen@gmail.com>
2021-06-03 10:22:40 +0200

    Minor updates

---
+++ cloud_practitioner/pandoc.sh
@@ -0,0 +1,7 @@
+
+pandoc \
+ --variable geometry:margin=1.5cm \
+ --variable fontsize=12pt \
+ --variable colorlinks=true \
+ aws_certified_cloud_practitioner.md -o aws_certified_cloud_practitioner.pdf
+
